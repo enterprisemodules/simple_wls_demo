e803a0d348dfd8e5a3a9709abd3a366afb5caac1
Bernard Pietraga <bernard.pietraga@gmail.com>
2019-01-22 17:22:02 +0100

    Update ords install

--- modules/role/manifests/wls/ords_server.pp
+++ modules/role/manifests/wls/ords_server.pp
@@ -1,15 +1,12 @@
 # Install java, WebLogic and create the domain.
 class role::wls::ords_server()
 {
-  contain ::profile::base
-  contain ::wls_profile::admin_server
   contain ::wls_install::ords::software
   #
   # Next add the stuff needed to configute
   #
 
-  Class['::profile::base']
-  -> Class['::wls_profile::admin_server']
-  -> Class['::wls_install::ords::software']
+  notice("run wls_install ords")
+  Class['::wls_install::ords::software']
 
 }
