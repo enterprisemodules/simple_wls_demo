4377d61c40f7b7b5fd6dae716042a1fee4fc10f1
Bernard Pietraga <bernard.pietraga@gmail.com>
2019-02-24 11:12:23 +0100

    Disable ords install

--- modules/role/manifests/wls/server.pp
+++ modules/role/manifests/wls/server.pp
@@ -2,11 +2,12 @@ class role::wls::server()
 {
   contain ::profile::base
   contain ::wls_profile::node
-  contain ::wls_install::ords::software
-  Class['::profile::base'] -> 
-  Class['::wls_profile::node'] -> 
-  Class['::wls_install::ords::software']
-  notice("run wls_install ords")
+  # ORDS disabled for development purposes
+  # contain ::wls_install::ords::software
+  Class['::profile::base'] ->
+  Class['::wls_profile::node']
+  # -> Class['::wls_install::ords::software']
+  # notice("run wls_install ords")
 }
 
- 
+
